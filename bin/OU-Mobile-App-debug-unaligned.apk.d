d0fba4e7122d753b644108c200e173a9a3e146d0
David Findley <thefindley@gmail.com>
2012-04-13 10:44:08 -0500

    Added the bin directory to the proejct

---
+++ bin/OU-Mobile-App-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/david/Workspace/OU-Mobile-App/bin/OU-Mobile-App-debug-unaligned.apk : \
+/Users/david/Workspace/OU-Mobile-App/bin/OU-Mobile-App.ap_ \
+/Users/david/Workspace/OU-Mobile-App/bin/classes.dex \
+/Users/david/android-sdk-macosx/tools/support/annotations.jar \
