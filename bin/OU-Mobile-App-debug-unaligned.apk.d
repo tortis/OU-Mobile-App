20e2c2d2873137502e426cf86238f4bc9976ff74
David Findley <thefindley@gmail.com>
2012-04-19 15:54:27 -0500

    Testing

--- bin/OU-Mobile-App-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/david/Workspace/OU-Mobile-App/bin/OU-Mobile-App-debug-unaligned.apk : \
-/Users/david/Workspace/OU-Mobile-App/bin/OU-Mobile-App.ap_ \
-/Users/david/Workspace/OU-Mobile-App/bin/classes.dex \
-/Users/david/android-sdk-macosx/tools/support/annotations.jar \
