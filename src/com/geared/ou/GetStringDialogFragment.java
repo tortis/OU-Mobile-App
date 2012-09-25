8848c61248eec3579c3e287687ca705fd684fae4
David Findley <thefindley@gmail.com>
2012-09-24 23:30:33 -0500

    Handy Dialog fragment for setting credientials

---
+++ src/com/geared/ou/GetStringDialogFragment.java
@@ -0,0 +1,130 @@
+package com.geared.ou;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.SherlockDialogFragment;
+import com.slidingmenu.lib.app.SlidingFragmentActivity;
+
+public class GetStringDialogFragment extends SherlockDialogFragment implements View.OnClickListener {
+	
+	OUApplication app;
+	private Context c;
+    private NewsActivity a;
+    
+    String title;
+    String oldData;
+    EditText mTextField;
+    
+    private static final int OK_BUTTON = 1;
+    private static final int CANCEL_BUTTON = 1;
+    
+    public void setTitle(String title, String oldData)
+    {
+		this.title = title;
+		this.oldData = oldData;
+    }
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		a = (NewsActivity)getActivity();
+		c = a.getApplicationContext();
+		app = (OUApplication) a.getApplication();
+		getDialog().setTitle(title);
+		
+		mTextField = new EditText(c);
+		mTextField.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, 55));
+		mTextField.setTextSize(20);
+		mTextField.setPadding(10, 0, 5, 0);
+		mTextField.setText(oldData);
+		mTextField.setTextColor(Color.BLACK);
+		if (title == "Password")
+			mTextField.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_PASSWORD);
+		
+		TextView ok = new TextView(c);
+		TextView cancel = new TextView(c);
+		
+		ok.setLayoutParams(new TableRow.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 0.5f));
+		cancel.setLayoutParams(new TableRow.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 0.5f));
+		
+		ok.setBackgroundResource(R.drawable.content_list_button_selector);
+		ok.setText("OK");
+		ok.setOnClickListener(this);
+		ok.setId(OK_BUTTON);
+		ok.setClickable(true);
+		ok.setTextColor(Color.BLACK);
+		ok.setTextSize(14);
+		ok.setGravity(Gravity.CENTER);
+		ok.setPadding(0, 20, 0, 20);
+		
+		cancel.setBackgroundResource(R.drawable.content_list_button_selector);
+		cancel.setText("Cancel");
+		cancel.setOnClickListener(this);
+		cancel.setId(CANCEL_BUTTON);
+		cancel.setClickable(true);
+		cancel.setTextColor(Color.BLACK);
+		cancel.setTextSize(14);
+		cancel.setGravity(Gravity.CENTER);
+		cancel.setPadding(0, 20, 0, 20);
+		
+		LinearLayout buttonContainer = new LinearLayout(c);
+		buttonContainer.setOrientation(LinearLayout.HORIZONTAL);
+		LinearLayout tlc = new LinearLayout(c);
+		tlc.setOrientation(LinearLayout.VERTICAL);
+		
+		View horizontalSpacer = new View(c);
+		horizontalSpacer.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, 1));
+		horizontalSpacer.setBackgroundColor(Color.LTGRAY);
+		
+		View horizontalSpacerBlank = new View(c);
+		horizontalSpacerBlank.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, 10));
+		
+		View verticalSpacer = new View(c);
+		verticalSpacer.setLayoutParams(new LayoutParams(1, LayoutParams.MATCH_PARENT));
+		verticalSpacer.setBackgroundColor(Color.LTGRAY);
+		
+		buttonContainer.addView(cancel);
+		buttonContainer.addView(verticalSpacer);
+		buttonContainer.addView(ok);
+		
+		tlc.addView(mTextField);
+		tlc.addView(horizontalSpacerBlank);
+		tlc.addView(horizontalSpacer);
+		tlc.addView(buttonContainer);
+		
+		return tlc;
+	}
+    
+	public void onClick(View v) {
+		if (v.getId() == OK_BUTTON)
+		{
+			SharedPreferences prefs = app.getPrefs();
+	    	SharedPreferences.Editor mEditor = prefs.edit();
+	    	if (title == "Username")
+	    		mEditor.putString("username", mTextField.getText().toString());
+	    	if (title == "Password")
+	    		mEditor.putString("password", mTextField.getText().toString());
+	    	mEditor.commit();
+	    	a.login();
+			this.dismiss();
+		}
+		else
+		{
+			this.dismiss();
+		}
+	}
+
+}
