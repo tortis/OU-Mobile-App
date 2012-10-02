e8768c890e2a50244869f7ed0eb6961d6ee88771
David <thefindley@gmail.com>
2012-10-02 10:23:11 -0500

    Rough draft of about page

--- src/com/geared/ou/AboutFragment.java
+++ src/com/geared/ou/AboutFragment.java
@@ -42,3 +42,35 @@ public class AboutFragment extends SherlockFragment {
         return test;
     }
 }
+/*
+OU Mobile App
+This android app was developed for free by David Findley for use by students at the University of Oklahoma.
+It lets users read OU Daily News, search for buildings on the campus map, and gives partial D2L access.  This is not
+an official University of Oklahoma application, nor is it sponsored by the University of Oklahoma.
+
+Special Thanks
+Design input: Vincent Do, Hoai Nguyen
+Art asset contribution: John Dewberry
+Testing device: Vincent Do
+
+License
+The OU Mobile App by David findley is licensed under a Creative Commons Attribution-NonCommercial 3.0 Unported License.(link)
+Based on work at github.com(link).
+
+Libraries Used
+This application uses several third-party opensource libraries in order implement it's features.
+
+	Apache HttpClient (link) is a library that lets developers interact with Web servers in a very robust way. OU Mobile App
+	uses this library to pull user specific Webpages from OU's D2L Website. This library is licensed under the
+	Apache License, version 2.0 (link).
+	
+	jsoup Html parser(link) is a library for reading and modifying html source files. OU Mobile App uses this 
+	library to extract user specific data from D2L Webpages. This library is licensed under the MIT License(link).
+	
+	ActionBarSherlock(link) is a library that extends the compatibility library in order to facilitate use of 
+	the action bar design patter across all versions of Android with a single API. This library is licensed 
+	under the Apache License, version 2.0 (link).
+	
+	SlidingMenu(link) is a library that allows Android apps to easily implement the sliding menu design pattern.
+	This library is licensed under Apache License, version 2.0 (link).
+	
